28368ef56b046a1bee6e5e2e554ac6171fd6a1ce
Spyros Argalias <sargalias@gmail.com>
2018-05-22 21:08:36 +0100

    set up google auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/auth/google', function(req, res, next) {
-    res.send('respond with a resource');
-});
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login']
+}));
 
 router.get('/auth/google/callback',
     passport.authenticate('google', {failureRedirect: '/login'}),
