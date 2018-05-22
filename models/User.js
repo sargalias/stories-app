b5d68f3ce3b778508c84d8785ed417d5cda0b538
Spyros Argalias <sargalias@gmail.com>
2018-05-22 21:37:46 +0100

    enforce unique googleId on user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    googleId: {type: String, required: true},
+    googleId: {type: String, required: true, unique: true},
 });
 
 module.exports = mongoose.model('User', userSchema);
