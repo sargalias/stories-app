dec02549663eb825fb9ce3b589bd45f682694c07
Spyros Argalias <sargalias@gmail.com>
2018-05-23 17:08:45 +0100

    embed stories and comments to user, add reference to story on comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
     authorId: {type: String, required: true},
+    storyId: {type: String, required: true},
     body: {type: String, required: true},
     date: {type: Date, default: Date.now, required: true}
 });
--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const Comment = require('./Comment');
+const Story = require('./Story');
 
 const userSchema = new mongoose.Schema({
     googleId: {type: String, required: true, unique: true},
-    name: {type: String, required: true}
+    name: {type: String, required: true},
+    comments: [Comment.schema],
+    stories: [Story.schema]
 });
 
 module.exports = mongoose.model('User', userSchema);
