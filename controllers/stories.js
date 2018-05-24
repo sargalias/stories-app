5e17a5c241fd65f9bb9498735d3d0da5e6a25161
Spyros Argalias <sargalias@gmail.com>
2018-05-24 03:05:56 +0100

    work on stories index view

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,4 +1,13 @@
+const Story = require('../models/Story');
 
 module.exports.index = (req, res, next) => {
-    res.render('stories/index');
+    Story
+        .find({privacy: 'PUBLIC'})
+        .sort({published: -1})
+        .exec((err, stories) => {
+            if (err) {
+                return next(err);
+            }
+            res.render('stories/index', {stories: stories});
+        });
 };
