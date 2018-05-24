f4b86e47eb1ed4836791614a60c87d11af527dc2
Spyros Argalias <sargalias@gmail.com>
2018-05-24 16:21:57 +0100

    work on story validation

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,4 +1,6 @@
 const Story = require('../models/Story');
+const {checkSchema, validationResult} = require('express-validator/check');
+
 
 module.exports.index = (req, res, next) => {
     Story
@@ -17,11 +19,63 @@ module.exports.new = (req, res, next) => {
     res.render('stories/new');
 };
 
+module.exports.storyValidation = checkSchema({
+    title: {
+        in: ['body'],
+        trim: true,
+        errorMessage: 'Title is required',
+        isLength: {
+            options: {min: 1}
+        },
+        escape: true
+    },
+    privacy: {
+        in: ['body'],
+        errorMessage: 'Invalid choice for status',
+        exists: true,
+        custom: {
+            options: (val, {req}) => {
+                return val.match(/^(PUBLIC|PRIVATE|UNLISTED)$/) != null;
+            }
+        },
+        escape: true
+    },
+    // if allowComments exists, it must equal true
+    allowComments: {
+        in: ['body'],
+        errorMessage: 'Invalid value for Allow Comments',
+        optional: true,
+        custom: {
+            options: (val, {req}) => {
+                return val.match(/^true$/) != null;
+            }
+        },
+        escape: true
+    },
+    body: {
+        in: ['body'],
+        errorMessage: 'Body is required',
+        trim: true,
+        isLength: {
+            options: {min: 1}
+        },
+        custom: {
+            options: (val, {req}) => {
+                return val.match(/^<p>&nbsp;<\/p>$/) === null;
+            }
+        }
+    }
+});
+
 module.exports.create = (req, res, next) => {
     // Takes html.
     // Must remove script tags
     // And save HTML as text
     // Then show the text as HTML
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+    }
     res.send(req.body);
 };
 
