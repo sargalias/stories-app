56d63c02e507f346e1b5c93ed33bccdc1f876eb9
Spyros Argalias <sargalias@gmail.com>
2018-05-25 01:20:39 +0100

    create authentication middleware

---
+++ helpers/authentication.js
@@ -0,0 +1,51 @@
+const Story = require('../models/Story');
+
+const userOwnsStory = (req, res, next) => {
+    for (let story of req.user.stories) {
+        if (story._id.equals(req.params.story_id)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const isStoryPrivate = (req, res, next) => {
+    Story.findById(req.params.story_id, (err, story) => {
+        if (err) {
+            return next(err);
+        }
+        if (!story) {
+            let err = new Error('Story not found');
+            err.statusCode = 404;
+            return next(err);
+        }
+        return story.privacy === 'PRIVATE';
+    });
+};
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (req.user) {
+        return next();
+    } else {
+        req.flash('alert', 'Please login');
+        res.redirect('/');
+    }
+};
+
+module.exports.ensureUserOwnsStory = (req, res, next) => {
+    if (userOwnsStory(req, res, next)) {
+        return next();
+    } else {
+        req.flash('alert', 'Not authorized');
+        res.redirect('/stories');
+    }
+};
+
+module.exports.ensureStoryNotPrivateOrUserOwnsStory = (req, res, next) => {
+    if (userOwnsStory(req, res, next) || !isStoryPrivate(req, res, next)) {
+        return next();
+    } else {
+        req.flash('alert', 'Not authorized');
+        res.redirect('/stories');
+    }
+};
