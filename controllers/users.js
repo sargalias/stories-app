657ed47ae7a60d1084da76485c7dedfb9db2602b
Spyros Argalias <sargalias@gmail.com>
2018-05-25 01:56:02 +0100

    add view for public stories by a specific user

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+const Story = require('../models/Story');
+const User = require('../models/User');
+
+module.exports.show = (req, res, next) => {
+    User.findById(req.params.user_id, (err, user) => {
+        let storiesToShow = user.stories.filter((story) => {
+            return story.privacy === 'PUBLIC';
+        });
+        res.render('users/show', {stories: storiesToShow});
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const uc = usersController = require('../controllers/users');
 
 /* GET users listing. */
 router.get('/dashboard', function(req, res, next) {
     res.render('users/dashboard');
 });
+
+router.get('/:user_id', uc.show);
 
 module.exports = router;
