0a9d3b9ddb7af4363e881d8f7dfd82ad80dff550
Spyros Argalias <sargalias@gmail.com>
2018-05-28 21:30:52 +0100

    add confirmation on story delete

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+const storyDeleteBtns = document.querySelectorAll('[data-story-delete]');
+for (let btn of storyDeleteBtns) {
+    btn.addEventListener('click', deleteStoryHandler);
+}
+
+function deleteStoryHandler(e) {
+    if (!confirm("Are you sure you want to delete this story?"))
+        e.preventDefault();
+}
