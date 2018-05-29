183435c6d2d5ed1fd489316606a1472104217f4d
Spyros Argalias <sargalias@gmail.com>
2018-05-29 03:23:05 +0100

    add new comments to user, stories and comment collections

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,7 +1,10 @@
 const Comment = require('../models/Comment');
+const Story = require('../models/Story');
+const User = require('../models/User');
 const {commentValidation} = require('../helpers/commentValidation');
 const {validationResult} = require('express-validator/check');
 const {matchedData} = require('express-validator/filter');
+const async = require('async');
 
 function createComment(req, res, next) {
     const errors = validationResult(req);
@@ -13,14 +16,38 @@ function createComment(req, res, next) {
         return res.redirect('back');
     }
     console.log(commentData);
-    Comment.create({
+    const comment = new Comment({
         storyId: req.params.story_id,
         body: commentData.commentText,
         author: req.user,
-    }, (err, comment) => {
-        if (err)
+    });
+    req.user.comments.push(comment);
+    Story.findById(req.params.story_id, (err, story) => {
+        if (err) {
             return next(err);
-        return res.redirect('back');
+        }
+        else if (!story) {
+            let err = new Error('Story not found');
+            err.statusCode = 404;
+            return next(err);
+        }
+        story.comments.push(comment);
+        async.parallel([
+            function (callback) {
+                story.save(callback);
+            },
+            function (callback) {
+                comment.save(callback);
+            },
+            function(callback) {
+                req.user.save(callback);
+            }
+        ], function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('back');
+        });
     });
 }
 
