457614cfd6354ec43d520cfe15a3095584e2c54e
Spyros Argalias <sargalias@gmail.com>
2018-05-30 02:07:31 +0100

    refactor main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,74 +1,74 @@
 $(document).foundation();
 
-const storyDeleteBtns = document.querySelectorAll('[data-story-delete]');
-for (let btn of storyDeleteBtns) {
-    btn.addEventListener('click', deleteStoryHandler);
+function storyHandler() {
+    const storyDeleteBtns = document.querySelectorAll('[data-story-delete]');
+    for (let btn of storyDeleteBtns) {
+        btn.addEventListener('click', deleteStoryHandler);
+    }
+
+    function deleteStoryHandler(e) {
+        if (!confirm("Are you sure you want to delete this story?"))
+            e.preventDefault();
+    }
 }
 
-function deleteStoryHandler(e) {
-    if (!confirm("Are you sure you want to delete this story?"))
-        e.preventDefault();
+function commentEditHandler() {
+    function applyEventListenerToNodes(nodes, eventType, func) {
+        for (let node of nodes) {
+            node.addEventListener(eventType, func);
+        }
+    }
+
+    const commentEditBtns = document.querySelectorAll('[data-comment-edit-btn]');
+    applyEventListenerToNodes(commentEditBtns, 'click', editComment);
+
+    const commentEditCancelBtns = document.querySelectorAll('[data-comment-cancel-btn]');
+    applyEventListenerToNodes(commentEditCancelBtns, 'click', cancelEditComment);
+
+    function getRelevantCommentNodes(id) {
+        return {
+            btnCont: document.querySelector(`[data-comment-button-cont="${id}"]`),
+            editCont: document.querySelector(`[data-comment-edit-form-cont="${id}"]`),
+            commentText: document.querySelector(`[data-comment-id="${id}"]`),
+            commentInput: document.querySelector(`[data-comment-input="${id}"]`)
+        }
+    }
+
+    function applyClass(nodes, className) {
+        for (let node of nodes) {
+            node.classList.add(className);
+        }
+    }
+
+    function removeClass(nodes, className) {
+        for (let node of nodes) {
+            node.classList.remove(className);
+        }
+    }
+
+    function editComment(e) {
+        const id = e.currentTarget.getAttribute('data-comment-edit-btn');
+        const {btnCont, editCont, commentText, commentInput} = getRelevantCommentNodes(id);
+
+        applyClass([commentText, btnCont], 'display-none');
+        removeClass([editCont], 'display-none');
+
+        // Give textarea correct body
+        commentInput.value = commentText.textContent.trim();
+    }
+
+    function cancelEditComment(e) {
+        const id = e.currentTarget.getAttribute('data-comment-cancel-btn');
+        const {btnCont, editCont, commentText, commentInput} = getRelevantCommentNodes(id);
+
+        removeClass([commentText, btnCont], 'display-none');
+        applyClass([editCont], 'display-none');
+    }
 }
 
-const commentEditBtns = document.querySelectorAll('[data-comment-edit]');
-for (let btn of commentEditBtns) {
-    btn.addEventListener('click', editComment);
-}
+document.addEventListener('DOMContentLoaded', main, false);
 
-const commentCancelEditBtns = document.querySelectorAll('[data-comment-cancel]');
-for (let btn of commentCancelEditBtns) {
-    btn.addEventListener('click', cancelEditComment);
-}
-
-const commentSubmitEditBtns = document.querySelectorAll('[data-comment-submit]');
-for (let btn of commentSubmitEditBtns) {
-    btn.addEventListener('click', submitEditComment);
-}
-
-
-// Get all edit buttons
-// Add an event listener to create a textarea.
-    // Hide the buttons and create two new buttons for submit, cancel.
-// If the textarea is submitted, an ajax request is made.
-// If ajax responds, reload the page.
-
-// Also hide the comment text when the editor comes up.
-
-function editComment(e) {
-    // hide the button container
-    const id = e.currentTarget.getAttribute('data-comment-edit');
-    const btnCont = document.querySelector(`[data-comment-button-cont="${id}"]`);
-    const editCont = document.querySelector(`[data-comment-edit-cont="${id}"]`);
-    const commentText = document.querySelector(`[data-comment-id="${id}"]`);
-
-
-    // Hide the comment body
-    commentText.classList.add('display-none');
-    // Hide button container
-    btnCont.classList.add('display-none');
-    // Unhide edit container
-    editCont.classList.remove('display-none');
-
-    // Give textarea correct body
-    const commentInput = document.querySelector(`[data-comment-input="${id}"]`);
-    commentInput.value = commentText.textContent.trim();
-}
-
-function cancelEditComment(e) {
-    console.log('clicked');
-    const id = e.currentTarget.getAttribute('data-comment-cancel');
-
-    const btnCont = document.querySelector(`[data-comment-button-cont="${id}"]`);
-    const editCont = document.querySelector(`[data-comment-edit-cont="${id}"]`);
-    const commentText = document.querySelector(`[data-comment-id="${id}"]`);
-
-    // Hide button container
-    commentText.classList.remove('display-none');
-    btnCont.classList.remove('display-none');
-    // Unhide edit container
-    editCont.classList.add('display-none');
-}
-
-function submitEditComment(e) {
-
+function main() {
+    storyHandler();
+    commentEditHandler();
 }
