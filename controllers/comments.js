b9ca0c7d5d0093affdc23ad3b1e2d1ed9caab32b
Spyros Argalias <sargalias@gmail.com>
2018-05-30 04:24:26 +0100

    add comment delete route

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,8 +4,9 @@ const {commentValidation} = require('../helpers/commentValidation');
 const {validationResult} = require('express-validator/check');
 const {matchedData} = require('express-validator/filter');
 const async = require('async');
+const mongoose = require('mongoose');
 
-function commentErrors(req, res, next) {
+function handleCommentErrors(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         for (let error of (errors.array())) {
@@ -17,7 +18,7 @@ function commentErrors(req, res, next) {
     }
 }
 
-function updateCommentsArray(subdocArray, updatedComment) {
+function updateCommentsArrayWithUpdatedComment(subdocArray, updatedComment) {
     for (let i=0; i<subdocArray.length; i++) {
         if (subdocArray[i]._id.equals(updatedComment.id)) {
             updateCommentWithNewCommentBody(subdocArray[i], updatedComment.body);
@@ -51,6 +52,11 @@ function saveCollections(collections, req, res, next) {
     );
 }
 
+function removeCommentReferenceFromArr(arr, referenceId) {
+    let i = arr.indexOf(referenceId);
+    arr.splice(i, 1);
+}
+
 function createComment(req, res, next) {
     const commentData = matchedData(req, {locations: ['body']});
     const comment = new Comment({
@@ -73,7 +79,7 @@ function createComment(req, res, next) {
     });
 }
 
-module.exports.create = [commentValidation, commentErrors, createComment];
+module.exports.create = [commentValidation, handleCommentErrors, createComment];
 
 function updateComment(req, res, next) {
     const commentData = matchedData(req, {locations: ['body']});
@@ -101,13 +107,36 @@ function updateComment(req, res, next) {
 
         // Update comment with new body
         updateCommentWithNewCommentBody(updatedComment, commentData.commentText);
-        updateCommentsArray(story.comments, updatedComment);
+        updateCommentsArrayWithUpdatedComment(story.comments, updatedComment);
         saveCollections([updatedComment, story], req, res, next);
     });
 }
 
-module.exports.update = [commentValidation, commentErrors, updateComment];
+module.exports.update = [commentValidation, handleCommentErrors, updateComment];
 
 module.exports.delete = (req, res, next) => {
-    res.send('Reached comment delete route');
+    async.parallel([
+        function(callback) {
+            Comment.findByIdAndRemove(req.params.comment_id, callback);
+        },
+        function(callback) {
+            Story.findById(req.params.story_id, (err, story) => {
+                if (err) {
+                    return callback(err);
+                }
+                story.comments.id(mongoose.Types.ObjectId(req.params.comment_id)).remove();
+                story.save(callback);
+            });
+        },
+        function(callback) {
+            removeCommentReferenceFromArr(req.user.comments, req.params.comment_id);
+            req.user.save(callback);
+        }
+    ], function(err, results) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Comment deleted');
+        res.redirect('back');
+    });
 };
